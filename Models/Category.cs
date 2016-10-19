2d60d0f0c530a54cd6d788b60d9c145d8f6ebfd4
GustafStark <gustafstark@gmail.com>
2016-10-19 10:25:29 +0200

    First

---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace HRMVer4.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CatergoryName { get; set; }
+
+        //Test
+    }
+}
